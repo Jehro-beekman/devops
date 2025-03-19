e2dcd3adc35bbdf62602dcc1646c8a0fdc584038
Jehro-beekman <2212815@talnet.nl>
2025-03-19 13:38:47 +0100

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -1,10 +1,10 @@
-import unittest 
+import unittest
 from app import add
-
+ 
 class TestApp(unittest.TestCase):
-  def test_add(self):
-    self.assertEqual(add(2, 3), 5)
-    self.assertEqual(add(-1, 1), 0)
-
-if __name__ == "_main_":
-  unittest.main()
+    def test_add(self):
+        self.assertEqual(add(2, 3), 5)
+        self.assertEqual(add(-1, 1), 0)
+ 
+if __name__ == "__main__":
+    unittest.main()
